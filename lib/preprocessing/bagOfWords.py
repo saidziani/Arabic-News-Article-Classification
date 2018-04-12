965d28219e1bac86cc629b65a8bb453f2518635c
Said <saidziani.pro@gmail.com>
2018-04-12 20:04:30 +0100

    Fixing path.

--- lib/preprocessing/bagOfWords.py
+++ lib/preprocessing/bagOfWords.py
@@ -9,21 +9,27 @@ help = Helper()
 
 raw = "/home/said/categ/data/raw" 
 temp = "/home/said/categ/data/temp" 
+root = "/media/said/DevStuff/PFE/ArabicTextCategorization/"
+# root = "/home/said/categ/"
+raw = root+'data/temp'
+temp = root+'data/temp'
+prep = root+'data/preprocessed'
 
 directories = os.listdir(raw)
 
 for directory in directories:
     directoryPath = os.path.join(raw, directory)
+    print(directoryPath)
     files = os.listdir(directoryPath)
     dic = {}
-    for i in range(1, len(files)+1):
+    for i in range(len(files)+1):
         file = str(i)
         filePath = os.path.join(directoryPath, file)
         bgw = help.getBagWordsArticle(filePath)
-        dic[file] = bgw
-        print(i)
-        w = i % 100
-        if w == 0 : 
-            help.setPickleContent(temp+'/'+directory+'/'+str(i), dic)
-            print('OK', i)
-
+        if bgw != [] :
+            dic[file] = bgw
+            print(i)
+            w = i % 50
+            if w == 0 :
+                help.setPickleContent(prep+'/'+directory+'/'+str(i), dic)
+                print('OK-- ', directory, i)
