61517ee3919237fc57c3d6923fb2a9dc84fb7f80
Said <saidziani.pro@gmail.com>
2018-04-12 08:53:54 +0100

    Update script to test just one file.é

--- lib/preprocessing/bagOfWords.py
+++ lib/preprocessing/bagOfWords.py
@@ -7,23 +7,8 @@ from helper import Helper
 help = Helper()
 
 
-raw = "/home/said/categ/data/raw" 
-temp = "/home/said/categ/data/temp" 
 
-directories = os.listdir(raw)
+filePath = '89'
+bgw = help.getBagWordsArticle(filePath)
 
-for directory in directories:
-    directoryPath = os.path.join(raw, directory)
-    files = os.listdir(directoryPath)
-    dic = {}
-    for i in range(1, len(files)+1):
-        file = str(i)
-        filePath = os.path.join(directoryPath, file)
-        bgw = help.getBagWordsArticle(filePath)
-        dic[file] = bgw
-        print(i)
-        w = i % 100
-        if w == 0 : 
-            help.setPickleContent(temp+'/'+directory+'/'+str(i), dic)
-            print('OK', i)
-
+print(bgw)
